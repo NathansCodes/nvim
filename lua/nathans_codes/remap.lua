8eb8708917c4b646730cf732d511665a33a66d76
Nathans-Codes <nat.schrei14@gmail.com>
2023-03-13 19:30:18 +0100

    idk

--- lua/nathans_codes/remap.lua
+++ lua/nathans_codes/remap.lua
@@ -37,5 +37,15 @@ vim.keymap.set("t", "<A-k>", "<C-\\><C-N><C-W>k")
 vim.keymap.set("t", "<A-l>", "<C-\\><C-N><C-W>l")
 
 vim.keymap.set("t", "<Esc>", "<C-\\><C-N>")
-vim.keymap.set("t", "<C-c>", "<C-\\><C-N>")
 
+vim.keymap.set(
+    {'n', 'v', 'i'}, "<C-j>", 
+    ":call vm#commands#add_cursor_down(0, v:count1)<cr>",
+    { noremap = true, silent = true }
+)   
+  
+vim.keymap.set(
+    {'n', 'v', 'i'}, "<C-k>", 
+    ":call vm#commands#add_cursor_up(0, v:count1)<cr>",
+    { noremap = true, silent = true } 
+) 
