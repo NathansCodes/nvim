721e789035469a3fdd7c68ef89ccdd3110a25914
Nathans-Codes <nat.schrei14@gmail.com>
2023-04-03 17:26:18 +0200

    made the status line wayyy better

---
+++ after/plugin/lualine.lua
@@ -0,0 +1,42 @@
+local custom_rose_pine = require('lualine.themes.rose-pine')
+
+custom_rose_pine.normal.c.bg = 'none'
+custom_rose_pine.visual.c.bg = 'none'
+custom_rose_pine.insert.c.bg = 'none'
+custom_rose_pine.command.c.bg = 'none'
+
+require('lualine').setup {
+    options = {
+        icons_enabled = true,
+        theme = custom_rose_pine,
+        component_separators = { left = '', right = ''},
+        section_separators = { left = '', right = ''},
+        disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = true,
+        refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
+        }
+    },
+    sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {},
+        lualine_x = {'encoding', 'filename'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
+    },
+    tabline = {},
+    winbar = {},
+    inactive_winbar = {},
+    extensions = {
+        'fugitive',
+        'nvim-dap-ui',
+    },
+}
--- lua/nathans_codes/packer.lua
+++ lua/nathans_codes/packer.lua
@@ -4,84 +4,89 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  -- telescope
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.x',
+    -- telescope
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.x',
 
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-  -- colors
-  use({
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  config = function()
-		  require("rose-pine").setup()
-		  vim.cmd('colorscheme rose-pine')
-	  end
-  })
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('nvim-treesitter/playground', {run = ':TSUpdate'})
-  use('nvim-treesitter/nvim-treesitter-context')
+    -- colors
+    use({
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            require("rose-pine").setup()
+            vim.cmd('colorscheme rose-pine')
+        end
+    })
+    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use('nvim-treesitter/playground', {run = ':TSUpdate'})
+    use('nvim-treesitter/nvim-treesitter-context')
 
-  -- for easy switching between files
-  use('theprimeagen/harpoon')
+    -- for easy switching between files
+    use('theprimeagen/harpoon')
 
-  -- undotree
-  use('mbbill/undotree')
+    -- undotree
+    use('mbbill/undotree')
 
-  -- for practice
-  use('theprimeagen/vim-be-good')
+    -- for practice
+    use('theprimeagen/vim-be-good')
 
-  -- multicursors
-  use('mg979/vim-visual-multi')
+    -- multicursors
+    use('mg979/vim-visual-multi')
 
-  -- lsp
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v1.x',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},             -- Required
-		  {'williamboman/mason.nvim'},           -- Optional
-		  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    -- lsp
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {'williamboman/mason.nvim'},           -- Optional
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},         -- Required
-		  {'hrsh7th/cmp-nvim-lsp'},     -- Required
-		  {'hrsh7th/cmp-buffer'},       -- Optional
-		  {'hrsh7th/cmp-path'},         -- Optional
-		  {'saadparwaiz1/cmp_luasnip'}, -- Optional
-		  {'hrsh7th/cmp-nvim-lua'},     -- Optional
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},         -- Required
+            {'hrsh7th/cmp-nvim-lsp'},     -- Required
+            {'hrsh7th/cmp-buffer'},       -- Optional
+            {'hrsh7th/cmp-path'},         -- Optional
+            {'saadparwaiz1/cmp_luasnip'}, -- Optional
+            {'hrsh7th/cmp-nvim-lua'},     -- Optional
 
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},             -- Required
-		  {'rafamadriz/friendly-snippets'}, -- Optional
-	  }
-  }
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},             -- Required
+            {'rafamadriz/friendly-snippets'}, -- Optional
+        }
+    }
 
-  -- debugging
-  use('nvim-lua/plenary.nvim')
-  use('mfussenegger/nvim-dap')
-  use('rcarriga/nvim-dap-ui')
-  use('theHamsta/nvim-dap-virtual-text')
-  use('nvim-telescope/telescope-dap.nvim')
-  use{'puremourning/vimspector', run = 'python3 install_gadget.py --all'}
-  
-  use {
-      'saecki/crates.nvim',
-      tag = 'v0.3.0',
-      requires = { 'nvim-lua/plenary.nvim' },
-      config = function()
-          require('crates').setup()
-      end,
-  }
+    -- debugging
+    use('nvim-lua/plenary.nvim')
+    use('mfussenegger/nvim-dap')
+    use('rcarriga/nvim-dap-ui')
+    use('theHamsta/nvim-dap-virtual-text')
+    use('nvim-telescope/telescope-dap.nvim')
+    use{'puremourning/vimspector', run = 'python3 install_gadget.py --all'}
 
-  -- git integration
-  use('tpope/vim-fugitive')
+    use {
+        'saecki/crates.nvim',
+        tag = 'v0.3.0',
+        requires = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            require('crates').setup()
+        end,
+    }
 
+    -- git integration
+    use('tpope/vim-fugitive')
+
+    -- beautification
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
 end)
 
