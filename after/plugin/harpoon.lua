62d97520c820431895383132641760db163be57c
Nathans-Codes <nat.schrei14@gmail.com>
2023-04-04 19:46:40 +0200

    changed harpoon keymaps again and added harpoon terminal keymap

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,10 +1,13 @@
+local term = require("harpoon.term")
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set({ "n", "t" }, "<C-e>", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<C-g>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-h>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-y>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-t>", function() ui.nav_file(4) end)
+vim.keymap.set({ "n", "t" }, "<C-u>", function() ui.nav_file(1) end)
+vim.keymap.set({ "n", "t" }, "<C-h>", function() ui.nav_file(2) end)
+vim.keymap.set({ "n", "t" }, "<C-n>", function() ui.nav_file(3) end)
+vim.keymap.set({ "n", "t" }, "<C-m>", function() ui.nav_file(4) end)
+
+vim.keymap.set("n", "<C-t>", function() term.gotoTerminal(1) end)
