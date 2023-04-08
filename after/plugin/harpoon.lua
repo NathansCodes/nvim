2baef66f8cd033d239dd7f0272eb4a8de3de4b22
Nathans-Codes <nat.schrei14@gmail.com>
2023-04-08 12:43:20 +0200

    I PROMISE you this is the last time I change the harpoon keymaps

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -5,9 +5,9 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set({ "n", "t" }, "<C-e>", ui.toggle_quick_menu)
 
-vim.keymap.set({ "n", "t" }, "<C-h>", function() ui.nav_file(1) end)
-vim.keymap.set({ "n", "t" }, "<C-u>", function() ui.nav_file(2) end)
-vim.keymap.set({ "n", "t" }, "<C-i>", function() ui.nav_file(3) end)
-vim.keymap.set({ "n", "t" }, "<C-o>", function() ui.nav_file(4) end)
+vim.keymap.set({ "n", "t" }, "<C-y>", function() ui.nav_file(1) end)
+vim.keymap.set({ "n", "t" }, "<C-h>", function() ui.nav_file(2) end)
+vim.keymap.set({ "n", "t" }, "<C-n>", function() ui.nav_file(3) end)
+vim.keymap.set({ "n", "t" }, "<C-b>", function() ui.nav_file(4) end)
 
 vim.keymap.set("n", "<C-t>", function() term.gotoTerminal(1) end)
