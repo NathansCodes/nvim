d1178395c749a0b6d762ad3b3c79b4d8562dfc1e
Nathans-Codes <nat.schrei14@gmail.com>
2023-04-10 10:26:02 +0200

    I swear this is the last time

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -7,7 +7,7 @@ vim.keymap.set({ "n", "t" }, "<C-e>", ui.toggle_quick_menu)
 
 vim.keymap.set({ "n", "t" }, "<C-y>", function() ui.nav_file(1) end)
 vim.keymap.set({ "n", "t" }, "<C-h>", function() ui.nav_file(2) end)
-vim.keymap.set({ "n", "t" }, "<C-n>", function() ui.nav_file(3) end)
+vim.keymap.set({ "n", "t" }, "<C-g>", function() ui.nav_file(3) end)
 vim.keymap.set({ "n", "t" }, "<C-b>", function() ui.nav_file(4) end)
 
 vim.keymap.set("n", "<C-t>", function() term.gotoTerminal(1) end)
