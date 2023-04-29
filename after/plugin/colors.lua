c3cfcb173da047d03852ff5b3addd1e0b340afba
Nathans-Codes <nat.schrei14@gmail.com>
2023-04-29 13:18:22 +0200

    fmt

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -5,10 +5,10 @@ function ColorMyVim(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" } )
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" } )
-    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" } )
-    vim.api.nvim_set_hl(0, "Menu", { bg = "none" } )
-    vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" } )
-    vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none" } )
+	vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" } )
+	vim.api.nvim_set_hl(0, "Menu", { bg = "none" } )
+	vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" } )
+	vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none" } )
 	vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" } )
 end
 
