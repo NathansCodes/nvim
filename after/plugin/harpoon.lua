3dd17fdde4ae6e15955fc3c42b0e7413d250280f
Nathans-Codes <nat.schrei14@gmail.com>
2023-04-29 13:21:17 +0200

    changed remaps for harpoon and splits to better work with tmux

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -3,11 +3,11 @@ local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set({ "n", "t" }, "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set({ "n", "t" }, "<A-e>", ui.toggle_quick_menu)
 
-vim.keymap.set({ "n", "t" }, "<C-y>", function() ui.nav_file(1) end)
-vim.keymap.set({ "n", "t" }, "<C-h>", function() ui.nav_file(2) end)
-vim.keymap.set({ "n", "t" }, "<C-g>", function() ui.nav_file(3) end)
-vim.keymap.set({ "n", "t" }, "<C-b>", function() ui.nav_file(4) end)
+vim.keymap.set({ "n", "t" }, "<A-h>", function() ui.nav_file(1) end)
+vim.keymap.set({ "n", "t" }, "<A-j>", function() ui.nav_file(2) end)
+vim.keymap.set({ "n", "t" }, "<A-k>", function() ui.nav_file(3) end)
+vim.keymap.set({ "n", "t" }, "<A-l>", function() ui.nav_file(4) end)
 
-vim.keymap.set("n", "<C-t>", function() term.gotoTerminal(1) end)
+vim.keymap.set("n", "<A-t>", function() term.gotoTerminal(1) end)
