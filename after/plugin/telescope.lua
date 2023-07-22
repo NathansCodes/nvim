21bfc04e2103c052752692098085107a86265769
NathansCodes <nat.schrei14@gmail.com>
2023-07-22 13:08:58 +0200

    telescope keymaps

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,5 @@
 local builtin = require('telescope.builtin')
+local actions = require('telescope.actions')
 
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
@@ -6,3 +7,21 @@ vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 
+require("telescope").setup {
+    defaults = {
+        mappings = {
+            i = {
+                ["<C-j>"] = actions.move_selection_next,
+                ["<C-k>"] = actions.move_selection_previous,
+                ["<C-l>"] = actions.select_default,
+                ["<C-u>"] = actions.results_scrolling_up,
+                ["<C-d>"] = actions.results_scrolling_down,
+            },
+            n = {
+                ["l"] = actions.select_default,
+                ["u"] = actions.results_scrolling_up,
+                ["d"] = actions.results_scrolling_down,
+            },
+        }
+    }
+}
