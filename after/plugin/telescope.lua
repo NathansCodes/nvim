4ca1e97853b632dd4b4ebdea5770d9c2173a5b1d
naan187 <nat.schrei14@gmail.com>
2023-10-12 19:09:11 +0200

    get input when doing a telescopr grep

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,7 +3,9 @@ local actions = require('telescope.actions')
 
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', builtin.grep_string, {})
+vim.keymap.set('n', '<leader>ps', function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
 
 require("telescope").setup {
     defaults = {
