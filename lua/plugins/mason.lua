b4c712124f93eada18bfd24ba62122d709557c3f
Nathan Schreitl <nat.schrei14@gmail.com>
2024-03-30 14:45:14 +0100

    actually mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,10 @@
 return {
-    -- "williamboman/mason.nvim",
-    -- build = ":MasonUpdate"
+    {
+        "williamboman/mason.nvim",
+        build = ":MasonUpdate"
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = { "williamboman/mason.nvim" },
+    }
 }
